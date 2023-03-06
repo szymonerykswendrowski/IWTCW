bc9c2b17250d9bef7469df6492fae860956ea9f4
Szymon Eryk Swendrowski <szymon.swendrowski@gmail.com>
2023-03-06 23:59:01 +0000

    Code structure and comments clean up.

--- iwt-cw.php
+++ iwt-cw.php
@@ -2,7 +2,9 @@
 
 header('Content-type: application/json');
 
-// Helper function for yearOp
+// Helper function for yearOp that returns
+// the appropriate operator between two
+// variables.
 function operand ($var1, $op, $var2) {
     switch ($op) {
         case "=":  return $var1 === $var2;
@@ -12,16 +14,17 @@ function operand ($var1, $op, $var2) {
     }
 }
 
+// Array to store results.
 $result = array();
 
-// File not selected error
+// File not selected error:
 if ($_GET['file'] === 'default') {
     $error = ["error" => "error, no file selected"];
     echo json_encode($error);
     exit();
 }
 
-// Case when only file is selected
+// Case when only file is selected:
 if (isset($_GET['file']) && $_GET['file'] != ''
     && empty($_GET['year']) && $_GET['tournament'] === 'Any'
     && empty($_GET['winner']) && empty($_GET['runner-up'])) {
@@ -40,6 +43,7 @@ if (isset($_GET['file']) && $_GET['file'] != ''
         echo json_encode($result);
 }
 
+// All the other cases:
 else {
     $file = $_GET['file'];
     $json = file_get_contents($file);
@@ -51,7 +55,10 @@ else {
     $winner = $_GET['winner'];
     $runnerUp = $_GET['runnerUp'];
 
-    // Year out of range error
+    // Year out of range error:
+    // ($year != '' at the end to ensure
+    // that when the form is cleared
+    // you don't get a false error).
     if (($file === 'mens-grand-slam-winners.json' && ($yearint < 1877 || $yearint > 2022) && $year != '')
         || ($file === 'womens-grand-slam-winners.json' && ($yearint < 1884 || $yearint > 2022) && $year != '')) {
         $error = ["error" => "error, year out of range"];
@@ -59,7 +66,7 @@ else {
         exit();
     }
 
-    // Case for all tournaments
+    // Case for all tournaments:
     if ($tournament === 'Any') {
         foreach($decoded_json as $item) {
                 if ((operand($item['year'], $yearOp, $yearint) || $year === "") 
@@ -75,7 +82,7 @@ else {
         } echo json_encode($result);  
     }
 
-    // Case for a specific tournament
+    // Case for a specific tournament:
     else {
         foreach($decoded_json as $item) {
         if ((operand($item['year'], $yearOp, $yearint) || $year === "") 
@@ -90,7 +97,9 @@ else {
                 ];
             }
         } echo json_encode($result);  
-    }       
+    }      
+    // || $i === '' conditions ensure that if year, winner or runnerUp
+    // are blank the query is still executed.
 }
 
 ?>
